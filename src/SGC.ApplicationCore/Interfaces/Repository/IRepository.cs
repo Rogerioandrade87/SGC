0b0870bdb015190a661fb847b2267b0eac714b86
Rogerio Andrade <rogerio_andrade1987@hotmail.com>
2020-06-24 16:50:54 -0300

    Aula 6

---
+++ src/SGC.ApplicationCore/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SGC.ApplicationCore.Interfaces.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        // TEntity é um tipo generico
+        TEntity Adicionar(TEntity entity);
+        void Atualizar(TEntity entity);
+        IEnumerable<TEntity> ObterTodos();
+        TEntity ObterPorId(int id);
+        //TEntity ObterPorId(string nome);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicado);//<== Expression Lambda
+        void Remover(TEntity entity);
+
+    }
+}
